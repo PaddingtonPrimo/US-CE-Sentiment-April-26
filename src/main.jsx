fe4a55d69d7bce8dd35d33146359f9abadaa25fb
PaddingtonPrimo <csiefker@2020companies.com>
2026-04-01 11:09:21 -0400

    Add vite config, index.html, and main entry point

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Dashboard from './consumer-electronics-sentiment'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Dashboard />
+  </React.StrictMode>
+)
